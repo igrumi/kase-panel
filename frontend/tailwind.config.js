3680607b7adfea9e40be4a7b3dec37f5cc2c87f4
igrumi <ignacio.grumi@gmail.com>
2025-04-27 02:22:39 -0400

    coloring

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#967969',
+        'primary-light': '#B8A08A',
+        'primary-dark': '#6D594D',
+        background: '#ffffff',
+        'text-primary': '#000000',
+        'text-secondary': '#4B4B4B',
+        romance: '#F5F2EA',    
+        westar: '#D0C7C2',       
+        donkey: '#896B60',       
+        spice: '#6C4A3F',        
+        espresso: '#4A332D',  
+      },
+    },
   },
   plugins: [],
 }
+
