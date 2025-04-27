5676af976fed1f9236740a8e8f6940277a7f2f3f
igrumi <ignacio.grumi@gmail.com>
2025-04-26 21:07:23 -0400

    glorb deleted

--- frontend/src/pages/HomeSection.jsx
+++ frontend/src/pages/HomeSection.jsx
@@ -3,7 +3,7 @@ import zibzabImg from '../assets/zibzab.png';
 export default function HomeSection() {
   return (
     <section id="home" className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-b from-indigo-100 to-white pt-20">
-      <h1 className="text-4xl font-bold text-indigo-700 mb-4">it me glorb</h1>
+      <h1 className="text-4xl font-bold text-indigo-700 mb-4">it me</h1>
       <p className="text-lg text-gray-700 text-center max-w-xl">
         zib zib zib zab zab 
       </p>
