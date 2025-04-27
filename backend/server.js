8bf322da28ada3b780b9a2d0cc940b6af7d37504
igrumi <ignacio.grumi@gmail.com>
2025-04-26 20:14:40 -0400

    mongo env variables added

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 
-const PORT = 5000;
-const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/paneldb';
+const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_URL = process.env.MONGO_URL;
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +19,6 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'sup!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(MONGO_PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${MONGO_PORT}`);
 });
